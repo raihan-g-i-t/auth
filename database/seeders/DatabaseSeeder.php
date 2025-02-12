39729e98c85ecb47fd2b4e0abf42839ce3610325
raihank703 <raihank703@gmail.com>
2025-02-12 20:37:46 +0600

    feb 12

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 
 class DatabaseSeeder extends Seeder
@@ -24,5 +25,10 @@ class DatabaseSeeder extends Seeder
             StudentSeeder::class,
             UserSeeder::class
         ]);
+
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
